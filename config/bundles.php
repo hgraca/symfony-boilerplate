a116d8773b3145af43fc7ce86c4fccfa8ace75aa
Herberto Graca <herberto.graca@gmail.com>
2018-11-27 11:10:22 +0100

    Revert "Add dependency on Doctrine"
    This reverts commit 2bccac836702e6eb8f535009e188af007ef455b1.
    
    Because at this moment we can't use doctrine with the CLI
    optimizations because the bundles depend on the HttpKernel component
    (or the framework component, I don't recall), so we need to remove
    it completely.
    At some point we can improve on the CLI optimizations to use the
    extensions and copy the bundles extensions to this boilerplate so
    that doctrine can be used with all its usual config.
    
    If you are not building a CLI only app, you should remove this
    commit and the "Optimize for CLI" commit.

--- config/bundles.php
+++ config/bundles.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Hgraca\DoctrineTestDbRegenerationBundle\HgracaDoctrineTestDbRegenerationBundle::class => ['test' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
--- tests/Framework/AbstractIntegrationTest.php
+++ tests/Framework/AbstractIntegrationTest.php
@@ -18,9 +18,7 @@ declare(strict_types=1);
 namespace Acme\App\Test\Framework;
 
 use Acme\App\Test\Framework\Container\ContainerAwareTestTrait;
-use Acme\App\Test\Framework\Database\DatabaseAwareTestTrait;
 use Acme\App\Test\Framework\Mock\MockTrait;
-use Hgraca\DoctrineTestDbRegenerationBundle\EventSubscriber\DatabaseAwareTestInterface;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -31,11 +29,10 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Usually, for this, we need to get services out of the container, etc. This top class makes it easier.
  * Furthermore, integration tests need to boot the application and therefore they are slower than the Unit tests.
  */
-abstract class AbstractIntegrationTest extends KernelTestCase implements DatabaseAwareTestInterface, AppTestInterface
+abstract class AbstractIntegrationTest extends KernelTestCase implements AppTestInterface
 {
     use AppTestTrait;
     use ContainerAwareTestTrait;
-    use DatabaseAwareTestTrait;
     use MockeryPHPUnitIntegration;
     use MockTrait;
 
