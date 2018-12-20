20a08bedd2b04c2bb6e5064195cc81c72c38ea00
Herberto Graca <herberto.graca@gmail.com>
2018-12-20 19:55:23 +0100

    Improvements to the default symfony flex setup

--- src/Infrastructure/Framework/Symfony/Kernel.php
+++ src/Infrastructure/Framework/Symfony/Kernel.php
@@ -28,7 +28,9 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    private $appProjectDir;
 
     public function getCacheDir()
     {
@@ -42,7 +44,7 @@ class Kernel extends BaseKernel
 
     public function registerBundles()
     {
-        $contents = require $this->getProjectDir() . '/config/bundles.php';
+        $contents = require $this->getConfDir() . '/bundles.php';
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
                 yield new $class();
@@ -52,12 +54,12 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
-        $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
+        $container->addResource(new FileResource($this->getConfDir() . '/bundles.php'));
         // Feel free to remove the "container.autowiring.strict_mode" parameter
         // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
         $container->setParameter('container.autowiring.strict_mode', true);
         $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir() . '/config';
+        $confDir = $this->getConfDir();
 
         $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
         $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
@@ -67,10 +69,32 @@ class Kernel extends BaseKernel
 
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
-        $confDir = $this->getProjectDir() . '/config';
+        $confDir = $this->getConfDir();
 
         $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+    }
+
+    private function getConfDir(): string
+    {
+        return $this->getProjectDir() . '/config';
+    }
+
+    public function getProjectDir(): string
+    {
+        if ($this->appProjectDir === null) {
+            $r = new \ReflectionObject($this);
+            $dir = $appDir = \dirname($r->getFileName());
+            while (!file_exists($dir . '/bin/console')) {
+                if ($dir === \dirname($dir)) {
+                    return $this->appProjectDir = $appDir;
+                }
+                $dir = \dirname($dir);
+            }
+            $this->appProjectDir = $dir;
+        }
+
+        return $this->appProjectDir;
     }
 }
